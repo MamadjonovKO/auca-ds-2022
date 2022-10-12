4e78d95a60a1b0430eaba79283f22f2b148c2d64
Khojikobiljon Mamadjonov <mamadjomov_k@auca.kg>
2022-10-12 07:46:50 +0200

    fix lab02 p21

--- lab02/p21/main.cpp
+++ lab02/p21/main.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
 
 using namespace std;
 
 int main()
 {
+    iostream::sync_with_stdio(false);
 
     int biggestEnd = 0;
     int leftCoord, height, rightCoord;
